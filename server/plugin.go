19737e38b80ab0140f868826012a620b28f5d409
thorfernbacher <thorfernbacher@gmail.com>
2020-03-29 17:34:13 +0000

    adding optional message grouping

--- server/plugin.go
+++ server/plugin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/lunny/html2md"
@@ -15,6 +16,7 @@ import (
 	"strings"
 	"sync"
 	"time"
+	"unicode/utf8"
 )
 
 //const RSSFEED_ICON_URL = "./plugins/rssfeed/assets/rss.png"
@@ -102,19 +104,22 @@ func (p *RSSFeedPlugin) getHeartbeatTime() (int, error) {
 }
 
 func (p *RSSFeedPlugin) processSubscription(subscription *Subscription) error {
-
+	config := p.getConfiguration()
 	if len(subscription.URL) == 0 {
 		return errors.New("no url supplied")
 	}
 
+	var attachments []*model.SlackAttachment
 	if rssv2parser.IsValidFeed(subscription.URL) {
-		err := p.processRSSV2Subscription(subscription)
+		var err error
+		attachments, err = p.processRSSV2Subscription(subscription)
 		if err != nil {
 			return errors.New("invalid RSS v2 feed format - " + err.Error())
 		}
 
 	} else if atomparser.IsValidFeed(subscription.URL) {
-		err := p.processAtomSubscription(subscription)
+		var err error
+		attachments, err = p.processAtomSubscription(subscription)
 		if err != nil {
 			return errors.New("invalid atom feed format - " + err.Error())
 		}
@@ -122,27 +127,45 @@ func (p *RSSFeedPlugin) processSubscription(subscription *Subscription) error {
 		return errors.New("invalid feed format")
 	}
 
+	//Send as separate messages or group as few messages as possible
+	var groupedAttachments [][]*model.SlackAttachment
+	if config.GroupMessages {
+		var err error
+		groupedAttachments, err = p.groupAttachments(attachments)
+		if err != nil {
+			return err
+		}
+	} else {
+		groupedAttachments = p.padAttachments(attachments)
+	}
+
+	p.API.LogError(fmt.Sprintf("%i", len(groupedAttachments)))
+	for _, group := range groupedAttachments {
+
+		p.createBotPost(subscription.ChannelID, group, "custom_git_pr")
+	}
+
 	return nil
 }
 
-func (p *RSSFeedPlugin) processRSSV2Subscription(subscription *Subscription) error {
+func (p *RSSFeedPlugin) processRSSV2Subscription(subscription *Subscription) ([]*model.SlackAttachment, error) {
 	config := p.getConfiguration()
 
 	// get new rss feed string from url
 	newRssFeed, newRssFeedString, err := rssv2parser.ParseURL(subscription.URL)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// retrieve old xml feed from database
 	oldRssFeed, err := rssv2parser.ParseString(subscription.XML)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	items := rssv2parser.CompareItemsBetweenOldAndNew(oldRssFeed, newRssFeed)
-
-	for _, item := range items {
+	attachments := make([]*model.SlackAttachment, len(items))
+	for index, item := range items {
 		attachment := &model.SlackAttachment{
 			Title:     item.Title,
 			TitleLink: item.Link,
@@ -151,49 +174,50 @@ func (p *RSSFeedPlugin) processRSSV2Subscription(subscription *Subscription) err
 		if config.ShowDescription {
 			attachment.Text = html2md.Convert(item.Description)
 		}
-		p.createBotPost(subscription.ChannelID, attachment, "custom_git_pr")
+		attachments[index] = attachment
 	}
-
 	if len(items) > 0 {
 		subscription.XML = newRssFeedString
 		p.updateSubscription(subscription)
 	}
 
-	return nil
+	return attachments, nil
 }
 
-func (p *RSSFeedPlugin) processAtomSubscription(subscription *Subscription) error {
+func (p *RSSFeedPlugin) processAtomSubscription(subscription *Subscription) ([]*model.SlackAttachment, error) {
 	// get new rss feed string from url
 	newFeed, newFeedString, err := atomparser.ParseURL(subscription.URL)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// retrieve old xml feed from database
 	oldFeed, err := atomparser.ParseString(subscription.XML)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	items := atomparser.CompareItemsBetweenOldAndNew(oldFeed, newFeed)
 
-	for _, item := range items {
+	attachments := make([]*model.SlackAttachment, len(items))
 
+	for index, item := range items {
 		attachment := &model.SlackAttachment{
-			Title: item.Title,
-
+			Title:      item.Title,
+			Fallback:   item.Title,
 			AuthorName: item.Author.Name,
 			AuthorLink: item.Author.URI,
 			AuthorIcon: getGravatarIcon(item.Author.Email),
 		}
 
+		attachments[index] = attachment
 		for _, link := range item.Link {
 			if link.Rel == "alternate" {
 				attachment.TitleLink = link.Href
 			}
 		}
 
-		//currently not supported
+		//timestamp field currently unused by mattermost
 		if item.Published != "" {
 			attachment.Timestamp = string(item.Published)
 		} else {
@@ -212,7 +236,7 @@ func (p *RSSFeedPlugin) processAtomSubscription(subscription *Subscription) erro
 				"subscription_url", subscription.URL,
 				"item_title", item.Title)
 		}
-		p.createBotPost(subscription.ChannelID, attachment, "custom_git_pr")
+
 	}
 
 	if len(items) > 0 {
@@ -220,21 +244,80 @@ func (p *RSSFeedPlugin) processAtomSubscription(subscription *Subscription) erro
 		p.updateSubscription(subscription)
 	}
 
-	return nil
+	return attachments, nil
 }
 
-func (p *RSSFeedPlugin) createBotPost(channelID string, attachment *model.SlackAttachment, postType string) error {
-	attachments := []*model.SlackAttachment{
-		attachment,
+// # of characters Json encoded array of slack attachments must be smaller than POST_PROPS_MAX_RUNES
+func (p *RSSFeedPlugin) groupAttachments(attachments []*model.SlackAttachment) ([][]*model.SlackAttachment, error) {
+
+	start := 0
+	size := 2
+
+	//mattermost max runes +
+	groupedAttachments := make([][]*model.SlackAttachment, 0)
+
+	for end, attachment := range attachments {
+		encoded, err := json.Marshal(attachment)
+
+		if err != nil {
+			return nil, err
+		}
+
+		encodedSize := utf8.RuneCountInString(string(encoded))
+
+		if size > model.POST_PROPS_MAX_USER_RUNES {
+			if start == end {
+				//single attachment is too long, trim then add
+				diff := size - model.POST_PROPS_MAX_USER_RUNES
+				textOffset := len(attachment.Text) - diff
+
+				if textOffset > 0 {
+					attachment.Text = attachment.Text[:textOffset]
+				} else {
+					//if we cant trim log error and skip
+					p.API.LogError("Attachment was too large and text could not be trimmed")
+				}
+
+				start = end
+				size = 0
+			} else {
+				groupedAttachments = append(groupedAttachments, attachments[start:end])
+			}
+			start = end
+			size = 0
+		} else {
+			size = size + encodedSize + 1 //+1 for comma
+		}
 	}
+	if start <= len(attachments)-1 {
+		groupedAttachments = append(groupedAttachments, attachments[start:len(attachments)])
+	}
+
+	return groupedAttachments, nil
+}
+
+func (p *RSSFeedPlugin) padAttachments(attachments []*model.SlackAttachment) [][]*model.SlackAttachment {
+	result := make([][]*model.SlackAttachment, len(attachments))
+
+	for index, attachment := range attachments {
+		result[index] = []*model.SlackAttachment{attachment}
+	}
+	return result
+}
+
+func (p *RSSFeedPlugin) createBotPost(channelID string, attachments []*model.SlackAttachment, postType string) error {
 
 	post := &model.Post{
 		UserId:    p.botUserID,
 		ChannelId: channelID,
-		Message:   "",
-		Type:      postType,
+
+		Type: postType,
 	}
+
 	post.AddProp("attachments", attachments)
+
+	//str, _ := json.Marshal(attachments)
+	//post.Message = string(str)
 
 	if _, err := p.API.CreatePost(post); err != nil {
 		p.API.LogError(err.Error())
